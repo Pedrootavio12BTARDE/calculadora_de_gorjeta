1bc1f47d671bfbead53d660bfdacc4acd30f0ac9
pedromartins06072008 <pedromartins06072008@gmail.com>
2025-04-07 03:04:29 +0000

    criação do javascripit, da função para receber o valor da conta  e da função para receber o número de pessoas

---
+++ main.js
@@ -0,0 +1,14 @@
+let bill = 0
+let tipPercentage = 0
+let numberOfPeople = 0
+
+function receiveBillValue(){
+    bill = document.querySelector("#bill").valueAsNumber
+
+    console.log(bill)
+}
+
+function receiveNumberOfPeopleValue(){
+    numberOfPeople = document.querySelector("#people").valueAsNumber
+
+}
